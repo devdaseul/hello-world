c37fd8629fddcdecfc63b9169895fed853b40848
devdaseul <surlloo8@naver.com>
2025-04-14 03:11:42 +0900

    thymeleaf alert 기능 구현

--- src/main/java/com/lily/helloworld/controller/HelloWorld.java
+++ src/main/java/com/lily/helloworld/controller/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.lily.helloworld;
+package com.lily.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
